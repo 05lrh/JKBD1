8e2489d8b92e68f2b35f43ffbc40239efcf3adaa
05lrh <1422729576@qq.com>
2017-06-30 00:45:42 +0800

    闪屏返回

---
+++ app/src/main/java/lrh/jkbd/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package lrh.jkbd.activity;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+import lrh.jkbd.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void RandomTest(View view) {
+        Intent intent = new Intent(MainActivity.this,RandomTestActivity.class);
+        startActivity(intent);
+    }
+}
