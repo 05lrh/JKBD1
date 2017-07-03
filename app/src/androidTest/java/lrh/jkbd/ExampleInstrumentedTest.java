3a89cf175825f83f26ad1fd22e6aa70783f25ea4
05lrh <1422729576@qq.com>
2017-07-04 02:18:44 +0800

    考试界面中有判断题的答案只显示A，B选项和答案

--- app/src/androidTest/java/lrh/jkbd/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/lrh/jkbd/ExampleInstrumentedTest.java
@@ -1,154 +1,83 @@
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+ +import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+@@ -32,8 +33,9 @@
+private TextView subject,limitTime,questionCount,tv_loading;
+private TextView tv_id,tv_question,q1,q2,q3,q4;
+private ImageButton img_pic;
+        -    private LinearLayout linearLayout;
+        +    private LinearLayout linearLayout,ll_C,ll_D;
 private ProgressBar pb_loading;
+        +    private CheckBox cb_C,cb_D;
 
         Exam exam;
-        +    Question question;
-        List<Question> questionList;
-
-        IExamBiz iExamBiz;
-@@ -106,15 +107,22 @@ private void initData(){
+        Question question;
+@@ -107,7 +109,6 @@ private void initData(){
         Log.e("exam","exam为空！");
         }
 
-        -                questionList=ExamApplication.getInstance().getQuestionList();
-        -                if(questionList!=null){
-        -                    tv_id.setText(questionList.get(0).getId()+".");
-        -                    tv_question.setText(""+questionList.get(0).getQuestion());
-        -                    q1.setText(""+questionList.get(0).getItem1());
-        -                    q2.setText(""+questionList.get(0).getItem2());
-        -                    q3.setText(""+questionList.get(0).getItem3());
-        -                    q4.setText(""+questionList.get(0).getItem4());
-        -                    Picasso.with(this).load(questionList.get(0).getUrl()).into(img_pic);
-        +//                questionList=ExamApplication.getInstance().getQuestionList();
-        +                question=iExamBiz.getQuestion();
-        +                if(question!=null){
-        +                    tv_id.setText(iExamBiz.getIndex()+1+".");
-        +                    tv_question.setText(""+question.getQuestion());
-        +                    q1.setText(""+question.getItem1());
-        +                    q2.setText(""+question.getItem2());
-        +                    q3.setText(""+question.getItem3());
-        +                    q4.setText(""+question.getItem4());
-        +                    if(question.getUrl()!=null&&!question.getUrl().equals("")){
-        +                        img_pic.setVisibility(View.VISIBLE);
-        +                        Picasso.with(this).load(question.getUrl()).into(img_pic);
-        +                    }else{
-        +                        img_pic.setVisibility(View.GONE);
-        +                    }
+        -//                questionList=ExamApplication.getInstance().getQuestionList();
+        question=iExamBiz.getQuestion();
+        if(question!=null){
+        tv_id.setText(iExamBiz.getIndex()+1+".");
+@@ -116,6 +117,12 @@ private void initData(){
+        q2.setText(""+question.getItem2());
+        q3.setText(""+question.getItem3());
+        q4.setText(""+question.getItem4());
         +
-        }else{
-        Log.e("onCreate:question","questionList为空！");
-        }
-@@ -152,6 +160,16 @@ private void initView(){
+        +                    ll_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+        +                    ll_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+        +                    cb_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+        +                    cb_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+        +
+        if(question.getUrl()!=null&&!question.getUrl().equals("")){
+        img_pic.setVisibility(View.VISIBLE);
+        Picasso.with(this).load(question.getUrl()).into(img_pic);
+@@ -158,6 +165,11 @@ private void initView(){
+        linearLayout= (LinearLayout) findViewById(R.id.ll_loading);
+        tv_loading= (TextView) findViewById(R.id.tv_loading);
         pb_loading= (ProgressBar) findViewById(R.id.pb_loading);
-        }
-
-        +    public void pre(View view) {
-        +        iExamBiz.Pre();
-        +        initData();
-        +    }
         +
-        +    public void next(View view) {
-        +        iExamBiz.Next();
-        +        initData();
-        +    }
-        +
-class LoadExamBroadcast extends BroadcastReceiver{
-
-    @Override
-    View
-30  app/src/main/java/cn/ucai/jkbd/biz/ExamBiz.java
-    @@ -1,5 +1,9 @@
-            package cn.ucai.jkbd.biz;
-  
- +import java.util.List;
- +
-         +import cn.ucai.jkbd.ExamApplication;
- +import cn.ucai.jkbd.bean.Question;
-  import cn.ucai.jkbd.dao.ExamDao;
-  import cn.ucai.jkbd.dao.IExamDao;
-
-    @@ -10,28 +14,50 @@
-    public class ExamBiz implements IExamBiz {
-        IExamDao dao;
-  
- +    int index=0;
- +    List<Question> questionList;
- +
-        public ExamBiz() {
-            this.dao = new ExamDao();
-        }
- +    @Override
- +    public int getIndex() {
-            +        return index;
-            +    }
-
-        @Override
-        public void ExamStart() {
-            +        index=0;
-            dao.loadExamInfo();
-            dao.loadQuestionLists();
+        +        ll_C= (LinearLayout) findViewById(R.id.ll_C);
+        +        ll_D= (LinearLayout) findViewById(R.id.ll_D);
+        +        cb_C= (CheckBox) findViewById(R.id.cb_C);
+        +        cb_D= (CheckBox) findViewById(R.id.cb_D);
         }
 
-        @Override
-        public void Pre() {
-            -
-                    +        if(index>0){
-                +            index--;
-                +        }
-        }
+public void pre(View view) {
+        View
+        4  app/src/main/res/layout/activity_random.xml
+@@ -175,6 +175,7 @@
+</LinearLayout>
 
-        @Override
-        public void Next() {
-            -
-                    +        this.getQuestion();
-            +        if(index>=0&&index<questionList.size()-1){
-                +            index++;
-                +        }
-        }
+<LinearLayout
+ +            android:id="@+id/ll_C"
+         android:layout_width="match_parent"
+         android:layout_height="wrap_content">
+<TextView
+@@ -192,6 +193,7 @@
+</LinearLayout>
 
-        @Override
-        public void commit() {
-
-        }
- +
-         +    @Override
- +    public Question getQuestion() {
-            +        questionList=ExamApplication.instance.getQuestionList();
-            +        if(questionList.get(index)!=null){
-                +            return questionList.get(index);
-                +        }
-            +        return null;
-            +    }
-    }
-    View
-3  app/src/main/java/cn/ucai/jkbd/biz/IExamBiz.java
-    @@ -1,5 +1,6 @@
-            package cn.ucai.jkbd.biz;
-  
- +import cn.ucai.jkbd.bean.Question;
-  import cn.ucai.jkbd.dao.IExamDao;
-
-/**
- @@ -11,4 +12,6 @@
- public void Pre();
- public void Next();
- public void commit();
- +    public Question getQuestion();
- +    public int getIndex();
- }
- View
- 2  app/src/main/res/layout/activity_random.xml
- @@ -252,12 +252,14 @@
- android:layout_height="wrap_content"
- android:gravity="center">
- <Button
- +                android:onClick="pre"
- android:layout_width="wrap_content"
- android:layout_height="wrap_content"
- android:drawableLeft="@mipmap/exam_pre32x32"
- android:text="上一题"
- android:background="@null"/>
- <Button
- +                android:onClick="next"
- android:layout_width="wrap_content"
- android:layout_height="wrap_content"
- android:drawableRight="@mipmap/exam_next32x32"
+<LinearLayout
+ +            android:id="@+id/ll_D"
+         android:layout_width="match_parent"
+         android:layout_height="wrap_content">
+<TextView
+@@ -229,11 +231,13 @@
+        android:layout_marginRight="30dp"
+        android:text="B"/>
+<CheckBox
+ +                android:id="@+id/cb_C"
+         android:layout_width="wrap_content"
+         android:layout_height="wrap_content"
+         android:layout_marginRight="30dp"
+         android:text="C"/>
+<CheckBox
+ +                android:id="@+id/cb_D"
+         android:layout_width="wrap_content"
+         android:layout_height="wrap_content"
+         android:layout_marginRight="30dp"
