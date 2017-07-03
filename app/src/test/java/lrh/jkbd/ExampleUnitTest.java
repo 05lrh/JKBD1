f0c975121582530cdaabd8995f0f4efd5792e3e2
05lrh <1422729576@qq.com>
2017-07-04 01:58:41 +0800

    删除转换

--- app/src/test/java/lrh/jkbd/ExampleUnitTest.java
+++ app/src/test/java/lrh/jkbd/ExampleUnitTest.java
@@ -1,62 +1,23 @@
-package lrh.jkbd.utils;
-import android.util.Log;
-import com.google.gson.Gson;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.List;
-import cn.ucai.jkbd.bean.Exam;
-import cn.ucai.jkbd.bean.Result;
-
-
-public class ResultUtils {
-    static String UTF_8 = "utf-8";
-    public static Result getListResultFromJson(String jsonStr){
-        Result result = new Result();
-        Log.e("Utils","jsonStr="+jsonStr);
-        try {
-            if(jsonStr==null || jsonStr.isEmpty() || jsonStr.length()<3)return null;
-            JSONObject jsonObject = new JSONObject(jsonStr);
-            if(!jsonObject.isNull("error_code")) {
-                result.setError_code(jsonObject.getInt("error_code"));
-            }else if(!jsonObject.isNull("msg")){
-                result.setError_code(jsonObject.getInt("msg"));
-            }
-            if(!jsonObject.isNull("reason")) {
-                result.setReason(jsonObject.getString("reason"));
-            }else if(!jsonObject.isNull("result")){
-                result.setReason(jsonObject.getString("result"));
-            }
-            if(!jsonObject.isNull("result")) {
-                JSONArray array = jsonObject.getJSONArray("result");
-                if (array != null) {
-                    List<Exam> list = new ArrayList<Exam>();
-                    for (int i = 0; i < array.length(); i++) {
-                        JSONObject jsonGroupAvatar = array.getJSONObject(i);
-                        Exam ga = new Gson().fromJson(jsonGroupAvatar.toString(), Exam.class);
-                        list.add(ga);
-                    }
-                    result.setResult(list);
-                    return result;
-                }
-            }else{
-                JSONArray array = new JSONArray(jsonStr);
-                if (array != null) {
-                    List<Exam> list = new ArrayList<Exam>();
-                    for (int i = 0; i < array.length(); i++) {
-                        JSONObject jsonGroupAvatar = array.getJSONObject(i);
-                        Exam ga = new Gson().fromJson(jsonGroupAvatar.toString(), Exam.class);
-                        list.add(ga);
-                    }
-                    result.setResult(list);
-                    return result;
-                }
-            }
-            return result;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return  null;
-    }
-
-}
+* @param context        Any {@link android.content.Context}.
+        -   * @param transformation Any {@link com.bumptech.glide.load.Transformation} for
+        -   *                       {@link android.graphics.Bitmap}s.
+        -   * @see #optionalTransform(android.content.Context, com.bumptech.glide.load.Transformation)
+        -   * @see #optionalTransform(Class, com.bumptech.glide.load.Transformation)
+        -   *
+        -   * @deprecated See {@link #transform(Transformation)}.
+        -   */
+        -  @Deprecated
+ -  @GlideOption(staticMethodName = "bitmapTransform")
+ -  public CHILD transform(
+        -      @SuppressWarnings("unused") Context context, @NonNull Transformation<Bitmap> transformation) {
+        -    return transform(transformation);
+        -  }
+        -
+        -  /**
+ -   * Applies the given {@link com.bumptech.glide.load.Transformation} for
+ -   * {@link android.graphics.Bitmap Bitmaps} to the default types ({@link android.graphics.Bitmap},
+ -   * {@link android.graphics.drawable.BitmapDrawable}, and
+ -   * {@link com.bumptech.glide.load.resource.gif.GifDrawable})
+ -   * and throws an exception if asked to transform an unknown type.
+ -   *
+ -   * <p>This will override previous calls to {@link #dontTransform()}.
