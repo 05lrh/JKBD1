a7d428957d8c1a0483c6e33c4e57db4ab5095b96
05lrh <1422729576@qq.com>
2017-07-04 23:55:45 +0800

    实现倒计时功能

--- app/src/androidTest/java/lrh/jkbd/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/lrh/jkbd/ExampleInstrumentedTest.java
@@ -1,51 +1,141 @@
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
- +import android.widget.RadioButton;
-import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
-@@ -35,7 +36,7 @@
-private ImageButton img_pic;
-private LinearLayout linearLayout,ll_C,ll_D;
-private ProgressBar pb_loading;
-        -    private CheckBox cb_C,cb_D;
-        +    private RadioButton cb_A,cb_B,cb_C,cb_D;
 
-        Exam exam;
-        Question question;
-@@ -168,20 +169,32 @@ private void initView(){
+import java.util.List;
+ import java.util.Timer;
+ import java.util.TimerTask;
 
-        ll_C= (LinearLayout) findViewById(R.id.ll_C);
-        ll_D= (LinearLayout) findViewById(R.id.ll_D);
-        -        cb_C= (CheckBox) findViewById(R.id.cb_C);
-        -        cb_D= (CheckBox) findViewById(R.id.cb_D);
-        +
-        +        cb_A= (RadioButton) findViewById(R.id.cb_A);
-        +        cb_B= (RadioButton) findViewById(R.id.cb_B);
-        +        cb_C= (RadioButton) findViewById(R.id.cb_C);
-        +        cb_D= (RadioButton) findViewById(R.id.cb_D);
-        }
+import cn.ucai.jkbd.ExamApplication;
+import cn.ucai.jkbd.R;
+@@ -34,7 +36,7 @@
 
-public void pre(View view) {
-        +        //this.initRadioButton();
-        iExamBiz.Pre();
-        initData();
-        }
+public class RandomTest extends Activity {
+    private TextView subject,limitTime,questionCount,tv_loading;
+    private TextView tv_id,tv_question,q1,q2,q3,q4;
+    private TextView tv_id,tv_question,q1,q2,q3,q4,tv_time;
+    private ImageButton img_pic;
+    private LinearLayout linearLayout,ll_C,ll_D;
+    private ProgressBar pb_loading;
+    @@ -109,6 +111,8 @@ private void initData(){
+        subject.setText(""+exam.getSubjectTitle());
+        limitTime.setText(""+exam.getLimitTime());
+        questionCount.setText(""+exam.getQuestionCount());
 
-public void next(View view) {
-        +        //this.initRadioButton();
-        iExamBiz.Next();
-        initData();
-        }
+                                  initTimer(exam);
+    }else{
+        Log.e("exam","exam为空！");
+    }
+    @@ -161,6 +165,88 @@ public void onClick(View v) {
 
-        +    public void initRadioButton(){
-        +        cb_A.setChecked(false);
-        +        cb_B.setChecked(false);
-        +        cb_C.setChecked(false);
-        +        cb_D.setChecked(false);
-        +    }
-        +
-class LoadExamBroadcast extends BroadcastReceiver{
+    }
 
-    @Override
+    private void initExam(){
+                exam= ExamApplication.getInstance().getExam();
+                if(exam!=null){
+                        subject.setText(""+exam.getSubjectTitle());
+                        limitTime.setText(""+exam.getLimitTime());
+                        questionCount.setText(""+exam.getQuestionCount());
+                    }else{
+                        Log.e("initExam:exam","exam为空！");
+                   }
+            }
+
+             private void initQuestion(){
+                question=iExamBiz.getQuestion();
+                if(question!=null){
+                        tv_id.setText(iExamBiz.getIndex()+1+".");
+                        tv_question.setText(""+question.getQuestion());
+                        q1.setText(""+question.getItem1());
+                        q2.setText(""+question.getItem2());
+                        q3.setText(""+question.getItem3());
+                        q4.setText(""+question.getItem4());
+
+                                ll_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+                        ll_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+                        cb_C.setVisibility(question.getItem3().equals("")?View.GONE:View.VISIBLE);
+                        cb_D.setVisibility(question.getItem4().equals("")?View.GONE:View.VISIBLE);
+
+                                if(question.getUrl()!=null&&!question.getUrl().equals("")){
+                                img_pic.setVisibility(View.VISIBLE);
+                                Picasso.with(this).load(question.getUrl()).into(img_pic);
+                            }else{
+                                img_pic.setVisibility(View.GONE);
+                            }
+
+                                initRadioButton();
+
+                                String userAnswer=question.getUserAnswer();
+                        if(userAnswer!=null&&!userAnswer.equals("")){
+                                int a=Integer.parseInt(userAnswer)-1;
+                                rb[a].setChecked(true);
+                            }
+
+                            }else{
+                        Log.e("initQuestion:question","questionList为空！");
+                    }
+            }
+
+             private void initTimer(Exam exam) {
+                int sumTime=exam.getLimitTime()*60*1000;
+        //        int sumTime=1*60*1000;
+
+                                final long overTime=sumTime+System.currentTimeMillis();
+                final Timer timer=new Timer();
+                timer.schedule(new TimerTask() {
+             @Override
+             public void run() {
+                                long l=overTime-System.currentTimeMillis();
+                               final long min=l/1000/60;
+                                final long sec=l/1000%60;
+                                runOnUiThread(new Runnable() {
+                    @Override
+                     public void run() {
+                                                tv_time.setText("剩余时间:"+min+"分"+sec+"秒");
+                                            }
+                 });
+                            }
+         },0,1000);
+
+                        timer.schedule(new TimerTask() {
+             @Override
+             public void run() {
+                                        timer.cancel();
+                                        runOnUiThread(new Runnable() {
+                   @Override
+                     public void run() {
+                                                       commit(null);
+                                                    }
+                 });
+                                    }
+         },sumTime);
+
+                    }
+
+
+    private void initView(){
+        subject= (TextView) findViewById(R.id.subject);
+        @@ -173,6 +259,7 @@ private void initView(){
+            q2= (TextView) findViewById(R.id.tv_B);
+            q3= (TextView) findViewById(R.id.tv_C);
+            q4= (TextView) findViewById(R.id.tv_D);
+                    tv_time= (TextView) findViewById(R.id.tv_time);
+            img_pic= (ImageButton) findViewById(R.id.img_pic);
+
+            linearLayout= (LinearLayout) findViewById(R.id.ll_loading);
+            @@ -213,13 +300,17 @@ public void onClick(View v) {
+                public void pre(View view) {
+                    setUserAnswer();
+                    iExamBiz.Pre();
+                           initData();
+                            //initData();
+                                    initExam();
+                            initQuestion();
+                }
+
+            public void next(View view) {
+                setUserAnswer();
+                iExamBiz.Next();
+                       initData();
+                        //initData();
+                                initExam();
+                        initQuestion();
+            }
